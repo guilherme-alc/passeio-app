034136bb6176a637a64c0122cded1638f40c4d84
guilherme-alc <guilhermealc01@gmail.com>
2026-03-29 16:29:17 -0300

    Configura ambiente prod x homol

--- src/app/lugares/lugar.service.ts
+++ src/app/lugares/lugar.service.ts
@@ -2,20 +2,22 @@ import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Lugar } from './lugar';
 import { Observable } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LugarService {
+  apiUrl: string = environment.apiUrl;
 
   constructor(private client: HttpClient) { }
 
   public salvar(lugar: Lugar) : Observable<Lugar> {
-    return this.client.post<Lugar>('http://localhost:3000/lugares', lugar);
+    return this.client.post<Lugar>(`${this.apiUrl}/lugares`, lugar);
   }
 
   public listar() : Observable<Lugar[]> {
-    return this.client.get<Lugar[]>('http://localhost:3000/lugares');
+    return this.client.get<Lugar[]>(`${this.apiUrl}/lugares`);
   }
 
   public listarComFiltros(categoria: number | null, lugar: string) : Observable<Lugar[]> {
@@ -28,6 +30,6 @@ export class LugarService {
     if (lugar) {
       parametros = parametros.set('nome_like', lugar);
     }
-    return this.client.get<Lugar[]>('http://localhost:3000/lugares', { params: parametros });
+    return this.client.get<Lugar[]>(`${this.apiUrl}/lugares`, { params: parametros });
   }
 }
